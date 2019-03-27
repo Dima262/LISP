0cd56f4051e2b0aff67a155f22fcc1ee504d45ca
Dima262 <43411503+Dima262@users.noreply.github.com>
2019-03-27 16:24:00 +0300

    Create Task3.lisp

---
+++ Task3.lisp
@@ -0,0 +1,11 @@
+;3.Определите функцию, заменяющую в исходном списке все вхождения заданного значения другим.
+
+(print(defun razd (n m w)
+(cond ((null w) nil)
+((equal (car w) m) (cons n (razd n m (cdr w))))
+((cons (car w) (razd n m (cdr w)))))
+       )
+)
+;Test
+(print(razd 6 7 '(2 4 3 6 7 )))
+==>(2 4 3 6 6) 
